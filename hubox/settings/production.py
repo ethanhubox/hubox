4612673707e50d3205854f532ed421ec34e0634d
miwooro <miwooro@hotmail.com>
2017-04-26 15:55:02 +0800

    production modified

--- hubox/settings/production.py
+++ hubox/settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.31.30.129',]
+ALLOWED_HOSTS = ['54.92.63.241',]
 
 
 # Application definition
